032b18bdb3019dba0676de36e27e8fb43308d2c7
Sebastián Benítez <sebastian@ds9soft.com>
2022-11-28 20:33:14 -0300

    Initial Commit

---
+++ FileAuditSystem/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FileAuditSystem
+//
+//  Created by Sebastián Benítez on 28/11/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
